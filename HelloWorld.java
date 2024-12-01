4f67e39619686207855c5482d6bed8c41d7774e6
Jack Fong <fhjfong@alumni.uwaterloo.ca>
2024-12-01 15:13:34 -0500

    first commit

---
+++ HelloWorld.java
@@ -0,0 +1,7 @@
+// File: HelloWorld.java
+public class HelloWorld {
+    public static void main(String[] args) {
+        // Print "Hello, World!" to the console
+        System.out.println("Hello, World!");
+    }
+}
